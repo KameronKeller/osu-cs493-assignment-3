6218b91f8e98d6ec90082ad30053c456df8e4587
KameronKeller <mrkameronkeller@gmail.com>
2024-05-11 12:36:16 -0700

    Add bcrypt

--- models/user.js
+++ models/user.js
@@ -1,14 +1,36 @@
+var bcrypt = require('bcryptjs');
 const { DataTypes } = require('sequelize')
 
 const sequelize = require("../lib/sequelize");
 const { Business } = require("./business");
 const { Review } = require("./review");
 
+async function hashPassword(password) {
+    return await bcrypt.hash(password, 8);
+}
+
 const User = sequelize.define('user', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-    admin: { type: DataTypes.BOOLEAN, allowNull: true }
+    admin: { type: DataTypes.BOOLEAN, allowNull: false }
+})
+
+User.beforeCreate(async (user, options) => {
+    const hashedPassword = await hashPassword(user.password);
+    console.log("== hashedPassword", hashedPassword);
+    user.password = hashedPassword;
+  });
+
+User.beforeBulkCreate( (users, options) => {
+    users.forEach( (user) => {
+        // hash synchronously on db init
+        // ref: https://stackoverflow.com/a/73513791/7100879
+        const hashedPassword = bcrypt.hashSync(user.password, 8);
+        console.log("== hashedPassword", hashedPassword);
+        user.password = hashedPassword
+        user.admin = false
+    })
 })
 
 User.hasMany(Business, { foreignKey: {name: 'ownerId', allowNull: false }})
